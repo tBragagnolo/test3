01805fc999a61a9ac547f07657b46c15b38d033e
tbragagnolo <tbragagnolo@myseneca.ca>
2022-11-22 18:58:49 -0500

    update

---
+++ data-service.js
@@ -0,0 +1,60 @@
+var fs = require("fs");
+
+var students = [];
+
+exports.prep = function prep(){
+    return new Promise((resolve, reject)=>{
+        fs.readFile("./students.json", (err, data)=>{
+            if(err) reject("Unable to read file");
+            students = JSON.parse(data);
+            resolve();
+        });
+    });
+}
+
+exports.cpa = function cpa(){
+    return new Promise((resolve, reject)=>{
+        var cpaStudents = [];
+
+        for(var i = 0; i < students.length; i++){
+            if(students[i].program == "CPA"){
+                cpaStudents.push(students[i]);
+            }
+        }
+
+        if(cpaStudents.length == 0) reject("No CPA Students");
+        else resolve(cpaStudents);
+    });
+}
+
+exports.bsd = function bsd(){
+    return new Promise((resolve, reject)=>{
+        var bsdStudents = [];
+
+        for(var i = 0; i < students.length; i++){
+            if(students[i].program == "BSD"){
+                bsdStudents.push(students[i]);
+            }
+        }
+
+        if(bsdStudents.length == 0) reject("No BSD Students");
+        else resolve(bsdStudents);
+    });
+}
+
+exports.highGPA = function highGPA(){
+    return new Promise((resolve, reject)=>{
+        var high = 0;
+        var hStudent;
+
+        for(var i = 0; i < students.length; i++){
+            if(students[i].gpa > high){
+                hStudent = students[i];
+                high = students[i].gpa;
+            }
+        }
+
+        if(!hStudent) reject("Cannot Find Student With Highest GPA");
+        else resolve(hStudent);
+    });
+}
---
+++ server.js
@@ -0,0 +1,72 @@
+var express = require("express");
+var dat = require("./data-service");
+
+var app = express();
+var port = process.env.PORT || 8081;
+
+function onStart(){
+    console.log("Express http server listening on", port);
+}
+
+app.get("/", (req, res)=>{
+    var resText = "<h2>Declaration: </h2>";
+    resText += "<p> The rest text is displayed in paragraph as shown in screenshot. </p>";
+    resText += " <p> I acknowledge the College’s academic integrity policy and my own integrity ";
+    resText += "remain in effect whether my work is done remotely or onsite.";
+    resText += " Any test or assignment is an act of trust between me and my instructor, ";
+    resText += " and especially with my classmates… even when no one is watching.";
+    resText += " I declare I will not break that trust. </p>";
+    resText += "<p>Name: <mark> <b> Tom Bragagnolo </b> </mark> </p>";
+    resText += "<p>Student Number: <mark><b> 139157218 </b> </mark> </p>";
+    
+    resText += `<p> <a href = "/BSD"> Click to visit BSD Students </a></p>
+                <p> <a href = "/highGPA"> Click to see who has the highest GPA </a></p>`;
+
+    res.send(resText);
+});
+
+app.get("/CPA", (req, res)=>{
+    dat.cpa().then((data)=>{
+        res.json(data);
+    }).catch((mes)=>{
+        res.json({message: mes});
+    });
+});
+
+app.get("/BSD", (req, res)=>{
+    dat.bsd().then((data)=>{
+        res.json(data);
+    }).catch((mes)=>{
+        res.json({message: mes});
+    });
+});
+
+app.get("/highGPA", (req, res)=>{
+    dat.highGPA().then((data)=>{
+        var resText = "<h2>Highest GPA: </h2>";
+        resText += "<p>Student ID: ";
+        resText += data.studId;
+        resText += "</p>";
+        resText += "<p>Name: ";
+        resText += data.name;
+        resText += "</p>";
+        resText += "<p>Program: ";
+        resText += data.program;
+        resText += "</p>";
+        resText += "<p>GPA: ";
+        resText += data.gpa;
+        resText += "</p>";
+
+        res.send(resText);
+    });
+});
+
+app.use("", (req, res)=>{
+    res.status(404).send("Error 404: Page Not Found");
+});
+
+dat.prep().then(()=>{
+    app.listen(port, onStart);
+}).catch((err)=>{
+    console.log(err);
+});
